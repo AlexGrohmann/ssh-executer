1245f5796a3285715ccd6005c8197390f43fd966
AlexGrohmann <alex.grohmann@googlemail.com>
2024-06-20 22:24:11 +0200

    try to auto commit

--- executer.sh
+++ executer.sh
@@ -5,18 +5,32 @@ USER="kali"
 HOST="192.168.178.98"
 COMMAND="cd ~/Documents/Test && npm install"
 
+# DE js_dev && styles extracten etc
 # Open SSH connection and execute command, capture output
 OUTPUT=$(ssh "${USER}@${HOST}" "${COMMAND}")
 
 # Check if the command was successful and respond based on the output
 if [[ "${OUTPUT}" == *"up to date"* ]]; then
     echo "Output contains up to date"
-    echo "execute next command"
-    echo $(ssh "${USER}@${HOST}" "${COMMAND}")
 else
-    echo "if statement was false"
+    echo "there was an error"
+    exit 1
     
 fi
+
+# committen
+# Open SSH connection and execute command, capture output
+COMMAND="cd ~/Documents/beauty-of-physics && git add . && git commit -m 'auto commit' && git push"
+OUTPUT=$(ssh "${USER}@${HOST}" "${COMMAND}")
+
+# Check if the command was successful and respond based on the output
+# if [[ "${OUTPUT}" == *"up to date"* ]]; then
+#     echo "Output contains up to date"
+# else
+#     echo "there was an error"
+#     exit 1
+
+# fi
 
 
 sleep 5
